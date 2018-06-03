f89088ca33df37bc7486e32e4a86bf682465d572
dravenxiaokai <jspankai@vip.qq.com>
2018-06-03 09:57:24 +0800

    用户模块schema

--- schemas/user.js
+++ schemas/user.js
@@ -3,11 +3,23 @@ var bcrypt = require('bcryptjs');
 var SALT_WORK_FACTOR = 10;
 
 var UserSchema = new mongoose.Schema({
+    email: {
+        unique: true,
+        type: String
+    },
     nickname: {
         unique: true,
         type: String
     },
     password: String,
+    sex: {
+        type: Number,
+        default: 1
+    },
+    phone: {
+        unique: true,
+        type: String
+    },
     role: {
         type: Number,
         default: 0
@@ -44,9 +56,27 @@ UserSchema.pre('save', function (next) {
             if (err) return next(err);
             user.password = hash;
             next();
-        })
-    })
+        });
+    });
 });
+// UserSchema.pre('update', function (next) {
+//     var user = this;
+//     // if (this.isNew) {
+//     //     this.meta.createAt = this.meta.updateAt = Date.now();
+//     // } else {
+//     //     this.meta.updateAt = Date.now();
+//     // }
+
+//     bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+//         if (err) return next(err);
+
+//         bcrypt.hash(user.password, salt, function (err, hash) {
+//             if (err) return next(err);
+//             user.password = hash;
+//             next();
+//         });
+//     });
+// });
 
 /**
  * 比较密码
