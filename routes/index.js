0eab466fdf371440e6d04c094509bcbab33ed36c
dravenxiaokai <jspankai@vip.qq.com>
2018-06-03 09:58:02 +0800

    index路由

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('user/signin', { title: '用户登录' });
 });
 
 module.exports = router;
